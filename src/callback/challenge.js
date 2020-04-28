16116cde52c41345151c2280dee377d0c0921129
Paul Osorio <osorpau@gmail.com>
2020-04-28 12:08:01 -0400

    se corrige llamado de api

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,6 +1,6 @@
 const { XMLHttpRequest } = require('xmlhttprequest');
 
-const API = 'https://rickandmortyapi.com/api/character';
+const API = 'https://rickandmortyapi.com/api/character/';
 const STATE_FINISHED_READY = 4;
 const STATUS_OK = 200;
 
@@ -8,9 +8,8 @@ function fetchData(urlApi, callback) {
   let xhttp = new XMLHttpRequest();
   xhttp.open('GET', urlApi, true);
   xhttp.onreadystatechange = function (event) {
-    
+
     if (xhttp.readyState === STATE_FINISHED_READY) {
-      console.log(xhttp);
       if (xhttp.status === STATUS_OK) {
         callback(null, JSON.parse(xhttp.responseText));
       } else {
@@ -23,9 +22,22 @@ function fetchData(urlApi, callback) {
 }
 
 fetchData(API, function (error1, data1) {
-  if (error1) return console.log(error1);
-  console.log(data1)
+  if (error1) return console.error(error1, 'error1');
+  //console.log(data1, 'data1')
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2, 'error2');
+    // console.log(data1.info, 'data1');
+    // console.log(data2, 'data2');
+    fetchData(data2.origin.url ,function (error3,data3) {
+      if (error3) return console.error(error3, 'error3');
+      console.log(data1.info.count, 'data1');
+      console.log(data2.name, 'data2');
+      console.log(data3.dimension, 'data3');
+    }
+    )
+  })
 });
+
 
 // fetchData(API, function (error1, data1) {
 //   if (error1) return console.error(error1, 'error1');
